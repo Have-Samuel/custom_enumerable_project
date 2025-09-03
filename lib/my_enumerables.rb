ed799e81421381c5b2d3bcd72d41e55a539f949d
samuel <samiehave9@gmail.com>
2025-09-03 12:45:22 +0300

    The each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,6 +1,16 @@
 module Enumerable
   # Your code goes here
+  def my_each
+    # Complete code here
+    self.each do |element|
+      return element if yield(element)
+    end
+    nil
+  end
 end
+
+a=[1,2,3,4,5]
+puts a.my_each { |x| x > 3 }
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
