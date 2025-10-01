6dba864e7b70e19c38e2d2f8af0a275fe4102b75
samuel <samiehave9@gmail.com>
2025-10-01 10:12:48 +0300

    After hybriding

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,32 +1,18 @@
-arr = [1,2,3,4,5]
 
-# module Enumerable
-  # Your code goes here
-  def my_each
-  i = 0
-    # Complete code here
-    until i == arr.length
-      yield(arr[i])
-      i += 1
-    end
-  end 
-# end
-
-
-my_each(arr) { |i| puts i * 2 }
-my_each(arr)
-
-def my_each_with_index
-  i = 0
-  until i == self.length
-    yield(self[i], i)
-    i += 1
-  end
-end
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    if block_given?
+      i = 0
+      until i == self.length do 
+        yield(self[i])
+        i += 1
+      end
+    end
+    self
+  end
 end
