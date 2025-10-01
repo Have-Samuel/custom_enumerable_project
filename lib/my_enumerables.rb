be592ec688f19d4c50a0e5b1887c0028d22d296f
samuel <samiehave9@gmail.com>
2025-10-01 10:28:09 +0300

    final Re-factoring

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,8 +1,35 @@
+# Final_Version
+module Enumerable
+  # Your code goes here
+  def my_each_with_index
+    if block_given?
+      i = 0
+      until i == self.length do 
+        yield(self[i], i)
+        i += 1
+      end
+    end
+    self
+  end
 
+  # MY_SELECT
+  def My_select
+    if block_given?
+      new_array = []
+      my_each { |i| new_array << i if yield(i) }
+      new_array
+    else
+      self
+    end
+
+    
+  end
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
+
+# After Re-factoring
 class Array
   # Define my_each here
   def my_each
