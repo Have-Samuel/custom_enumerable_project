799904ffeb2cfd65bfe8d216db689c53e4bfb69e
samuel <samiehave9@gmail.com>
2025-10-01 10:01:04 +0300

    Editing

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -11,7 +11,18 @@ arr = [1,2,3,4,5]
     end
   end 
 # end
-puts a.my_each { |x| x > 3 }
+
+
+my_each(arr) { |i| puts i * 2 }
+my_each(arr)
+
+def my_each_with_index
+  i = 0
+  until i == self.length
+    yield(self[i], i)
+    i += 1
+  end
+end
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
